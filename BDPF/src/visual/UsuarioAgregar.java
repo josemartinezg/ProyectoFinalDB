c7f89940f96f060e795302fe76235b814960d908
José M. Martínez García <jmlmartinezg@outlook.com>
2018-12-06 03:46:07 -0400

    Insert Funcional desde la ventana del usuario

--- BDPF/src/visual/UsuarioAgregar.java
+++ BDPF/src/visual/UsuarioAgregar.java
@@ -17,16 +17,52 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.SwingConstants;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 
 public class UsuarioAgregar extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	private JTextField txtIDPersona;
+	private JTextField txtP_Nombre;
+	private JTextField txtS_Nombre;
+	private JTextField txtP_Apellido;
+	private JTextField txtS_Apellido;
+	private JTextField txtContrasenna;
+	private JComboBox cbxDia;
+	private JComboBox cbxMes;
+	private JComboBox cbxAnno;
+	private JComboBox cbxGenero;
+	
+	/*private String ID_Persona;
+	private String P_Nombre; 
+	private String S_Nombre; 
+	private String P_Apellido; 
+	private String S_Apellido; 
+	private String dia; 
+	private String mes; 
+	private String anno; 
+	private String Fecha_Nacimiento; 
+	private String genero;
+	private String Pais;
+	private String Cedula;*/
+	private String Contrasenna;
+	
+	private Connection connect = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+    private JTextField txtPais;
+    private JTextField txtCedula;
+    
+
 
 	/**
 	 * Launch the application.
@@ -48,6 +84,8 @@ public class UsuarioAgregar extends JFrame {
 	 * Create the frame.
 	 */
 	public UsuarioAgregar() {
+		
+		setTitle("Registro de Usuario");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 477);
 		contentPane = new JPanel();
@@ -56,80 +94,108 @@ public class UsuarioAgregar extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblIdUsuario = new JLabel("Id Usuario");
-		lblIdUsuario.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblIdUsuario.setBounds(10, 33, 72, 14);
-		contentPane.add(lblIdUsuario);
+		JLabel lblID = new JLabel("ID:");
+		lblID.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblID.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblID.setBounds(28, 34, 72, 14);
+		contentPane.add(lblID);
 		
-		JLabel lblNombres = new JLabel("Nombres");
-		lblNombres.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNombres.setBounds(10, 102, 72, 14);
+		JLabel lblNombres = new JLabel("Nombres:");
+		lblNombres.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblNombres.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNombres.setBounds(12, 159, 72, 14);
 		contentPane.add(lblNombres);
 		
-		JLabel lblApellidos = new JLabel("Apellidos");
-		lblApellidos.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblApellidos.setBounds(10, 142, 72, 14);
+		JLabel lblApellidos = new JLabel("Apellidos:");
+		lblApellidos.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblApellidos.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblApellidos.setBounds(12, 198, 72, 19);
 		contentPane.add(lblApellidos);
 		
-		JLabel lblContrasena = new JLabel("Contrasena");
-		lblContrasena.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblContrasena.setBounds(10, 71, 72, 14);
+		JLabel lblContrasena = new JLabel("Contrase\u00F1a:");
+		lblContrasena.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblContrasena.setBounds(12, 74, 90, 14);
 		contentPane.add(lblContrasena);
 		
-		textField = new JTextField();
-		textField.setBounds(92, 32, 255, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtIDPersona = new JTextField();
+		txtIDPersona.setBounds(112, 32, 275, 20);
+		contentPane.add(txtIDPersona);
+		txtIDPersona.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(92, 101, 101, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(243, 101, 104, 20);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(92, 141, 101, 20);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
+		txtP_Nombre = new JTextField();
+		txtP_Nombre.setBounds(94, 157, 140, 20);
+		contentPane.add(txtP_Nombre);
+		txtP_Nombre.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(243, 141, 104, 20);
-		contentPane.add(textField_4);
-		textField_4.setColumns(10);
 		
-		textField_5 = new JTextField();
-		textField_5.setBounds(92, 70, 209, 20);
-		contentPane.add(textField_5);
-		textField_5.setColumns(10);
+		txtS_Nombre = new JTextField();
+		txtS_Nombre.setBounds(249, 157, 140, 20);
+		contentPane.add(txtS_Nombre);
+		txtS_Nombre.setColumns(10);
+		
+		
+		txtP_Apellido = new JTextField();
+		txtP_Apellido.setBounds(94, 197, 140, 20);
+		contentPane.add(txtP_Apellido);
+		txtP_Apellido.setColumns(10);
+		
+		
+		txtS_Apellido = new JTextField();
+		txtS_Apellido.setBounds(249, 198, 140, 20);
+		contentPane.add(txtS_Apellido);
+		txtS_Apellido.setColumns(10);
+		
+		
+		txtContrasenna = new JTextField();
+		txtContrasenna.setBounds(112, 72, 275, 20);
+		contentPane.add(txtContrasenna);
+		txtContrasenna.setColumns(10);
+		
 		
 		JButton btnAgregar = new JButton("Agregar");
 		btnAgregar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//Aqui se debe poner que se ingrese a la BD y que se cierre la ventana
+				try {
+					String ID_Persona = txtIDPersona.getText();
+					String P_Nombre = txtP_Nombre.getText();
+					String S_Nombre = txtS_Nombre.getText();
+					String P_Apellido = txtP_Apellido.getText();
+					String S_Apellido = txtS_Apellido.getText();
+					String Contrasenna = txtContrasenna.getText();
+					String dia = cbxDia.getSelectedItem().toString();
+					String mes = cbxMes.getSelectedItem().toString();
+					String anno = cbxAnno.getSelectedItem().toString();
+					String genero = cbxGenero.getSelectedItem().toString();
+					String Pais = txtPais.getText();
+					String Cedula = txtCedula.getText();
+					String Fecha_Nacimiento = anno + '-' + mes + '-' + dia;
+					readDataBase( ID_Persona, P_Nombre, S_Nombre, P_Apellido, S_Apellido, Fecha_Nacimiento, genero, Pais, Cedula);
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			
 			}
 		});
-		btnAgregar.setBounds(92, 404, 89, 23);
+		btnAgregar.setBounds(102, 369, 105, 35);
 		contentPane.add(btnAgregar);
 		
 		JLabel label = new JLabel("*");
-		label.setBounds(203, 104, 46, 14);
+		label.setBounds(239, 162, 21, 14);
 		contentPane.add(label);
 		
 		JLabel label_1 = new JLabel("*");
-		label_1.setBounds(203, 144, 46, 14);
+		label_1.setBounds(239, 203, 21, 14);
 		contentPane.add(label_1);
 		
 		JLabel lblCamposObligatorios = new JLabel("* Campos Obligatorios");
-		lblCamposObligatorios.setBounds(10, 379, 183, 14);
+		lblCamposObligatorios.setBounds(141, 346, 130, 14);
 		contentPane.add(lblCamposObligatorios);
 		
 		JLabel label_2 = new JLabel("*");
-		label_2.setBounds(311, 73, 46, 14);
+		label_2.setBounds(388, 72, 21, 14);
 		contentPane.add(label_2);
 		
 		JButton btnVolver = new JButton("Volver");
@@ -142,69 +208,213 @@ public class UsuarioAgregar extends JFrame {
 				
 			}
 		});
-		btnVolver.setBounds(237, 404, 89, 23);
+		btnVolver.setBounds(219, 369, 107, 35);
 		contentPane.add(btnVolver);
 		
-		JLabel lblFechaNacimiento = new JLabel("Fecha Nacimiento");
-		lblFechaNacimiento.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblFechaNacimiento.setBounds(10, 177, 140, 14);
+		JLabel lblFechaNacimiento = new JLabel("Fecha Nacimiento:");
+		lblFechaNacimiento.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblFechaNacimiento.setBounds(31, 263, 140, 14);
 		contentPane.add(lblFechaNacimiento);
 		
-		JLabel lblDia = new JLabel("Dia");
+		JLabel lblDia = new JLabel("Dia:");
 		lblDia.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblDia.setBounds(10, 202, 46, 14);
+		lblDia.setBounds(28, 290, 46, 14);
 		contentPane.add(lblDia);
 		
-		JLabel lblMes = new JLabel("Mes");
+		JLabel lblMes = new JLabel("Mes:");
 		lblMes.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblMes.setBounds(109, 202, 46, 14);
+		lblMes.setBounds(84, 290, 46, 14);
 		contentPane.add(lblMes);
 		
-		JLabel lblAnyo = new JLabel("Anyo");
-		lblAnyo.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblAnyo.setBounds(203, 202, 46, 14);
-		contentPane.add(lblAnyo);
+		JLabel lblAnno = new JLabel("A\u00F1o:");
+		lblAnno.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblAnno.setBounds(153, 290, 46, 14);
+		contentPane.add(lblAnno);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setEditable(true);
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
-		comboBox.setBounds(10, 227, 46, 20);
-		contentPane.add(comboBox);
+		cbxDia = new JComboBox();
+		cbxDia.setEditable(true);
+		cbxDia.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
+		cbxDia.setBounds(28, 313, 46, 20);
+		contentPane.add(cbxDia);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"}));
-		comboBox_1.setEditable(true);
-		comboBox_1.setBounds(98, 227, 57, 20);
-		contentPane.add(comboBox_1);
 		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setEditable(true);
+		
+		cbxMes = new JComboBox();
+		cbxMes.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
+		cbxMes.setEditable(true);
+		cbxMes.setBounds(85, 313, 57, 20);
+		contentPane.add(cbxMes);
+		
+		
+		cbxAnno = new JComboBox();
+		cbxAnno.setEditable(true);
 	
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"1900", "1901", "1902", "1903", "1904", "1905", "1906", "1907", "1908", "1909", "1910", "1911", "1912", "1913", "1914", "1915", "1916", "1917", "1918", "1919", "1920", "1921", "1922", "1923", "1924", "1925", "1926", "1927", "1928", "1929", "1930", "1931", "1932", "1933", "1934", "1935", "1936", "1937", "1938", "1939", "1940", "1941", "1942", "1943", "1944", "1945", "1946", "1947", "1948", "1949", "1950", "1951", "1952", "1953", "1954", "1955", "1956", "1957", "1958", "1959", "1960", "1961", "1962", "1963", "1964", "1965", "1966", "1967", "1968", "1969", "1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"}));
-		comboBox_2.setBounds(192, 227, 57, 20);
-		contentPane.add(comboBox_2);
+		cbxAnno.setModel(new DefaultComboBoxModel(new String[] {"1900", "1901", "1902", "1903", "1904", "1905", "1906", "1907", "1908", "1909", "1910", "1911", "1912", "1913", "1914", "1915", "1916", "1917", "1918", "1919", "1920", "1921", "1922", "1923", "1924", "1925", "1926", "1927", "1928", "1929", "1930", "1931", "1932", "1933", "1934", "1935", "1936", "1937", "1938", "1939", "1940", "1941", "1942", "1943", "1944", "1945", "1946", "1947", "1948", "1949", "1950", "1951", "1952", "1953", "1954", "1955", "1956", "1957", "1958", "1959", "1960", "1961", "1962", "1963", "1964", "1965", "1966", "1967", "1968", "1969", "1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"}));
+		cbxAnno.setBounds(153, 313, 57, 20);
+		contentPane.add(cbxAnno);
 		
-		JLabel lblGenero = new JLabel("Genero");
-		lblGenero.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblGenero.setBounds(10, 287, 72, 14);
+		
+		JLabel lblGenero = new JLabel("Genero:");
+		lblGenero.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblGenero.setBounds(246, 290, 72, 14);
 		contentPane.add(lblGenero);
 		
-		JComboBox comboBox_3 = new JComboBox();
-		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"Hombre", "Mujer"}));
-		comboBox_3.setEditable(true);
-		comboBox_3.setBounds(10, 312, 89, 20);
-		contentPane.add(comboBox_3);
+		
+		
+		cbxGenero = new JComboBox();
+		cbxGenero.setModel(new DefaultComboBoxModel(new String[] {"M", "F"}));
+		cbxGenero.setEditable(true);
+		cbxGenero.setBounds(246, 313, 89, 20);
+		contentPane.add(cbxGenero);
+		
 		
 		JLabel label_3 = new JLabel("*");
-		label_3.setBounds(55, 289, 46, 14);
+		label_3.setForeground(Color.RED);
+		label_3.setFont(new Font("Tahoma", Font.BOLD, 13));
+		label_3.setBounds(305, 291, 21, 14);
 		contentPane.add(label_3);
 		
 		JLabel label_4 = new JLabel("*");
-		label_4.setBounds(119, 179, 46, 14);
+		label_4.setForeground(Color.RED);
+		label_4.setFont(new Font("Tahoma", Font.BOLD, 13));
+		label_4.setBounds(156, 264, 15, 14);
 		contentPane.add(label_4);
 		
 		JLabel label_5 = new JLabel("*");
-		label_5.setBounds(355, 35, 46, 14);
+		label_5.setBounds(388, 34, 21, 14);
 		contentPane.add(label_5);
+		
+		JLabel lblPais = new JLabel("Pais:");
+		lblPais.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblPais.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblPais.setBounds(12, 231, 72, 19);
+		contentPane.add(lblPais);
+		
+		txtPais = new JTextField();
+		txtPais.setColumns(10);
+		txtPais.setBounds(94, 230, 293, 20);
+		contentPane.add(txtPais);
+		
+		
+		txtCedula = new JTextField();
+		txtCedula.setColumns(10);
+		txtCedula.setBounds(112, 114, 275, 20);
+		contentPane.add(txtCedula);
+		
+		
+		JLabel lblCedula = new JLabel("C\u00E9dula:");
+		lblCedula.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblCedula.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblCedula.setBounds(10, 116, 90, 14);
+		contentPane.add(lblCedula);
+		
 	}
+    public void readDataBase(String ID_Persona, String P_Nombre, String S_Nombre, String P_Apellido, String S_Apellido, String Fecha_Nacimiento, String genero, String Pais, String Cedula) throws Exception {
+        try {
+            // This will load the MySQL driver, each DB has its own driver
+            Class.forName("com.mysql.jdbc.Driver");
+            // Setup the connection with the DB
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/trabajofinaldb", "chema", "Nom@sde16");
+
+            // Statements allow to issue SQL queries to the database
+            statement = connect.createStatement();
+            // Result set get the result of the SQL query
+            resultSet = statement
+                    .executeQuery("select * from trabajofinaldb.persona");
+            writeResultSet(resultSet);
+
+            // PreparedStatements can use variables and are more efficient
+            preparedStatement = connect
+                    .prepareStatement("insert into trabajofinaldb.persona values (?, ?, ?, ?, ? , ?, ?, ?, ?)");
+            // "myuser, webpage, datum, summary, COMMENTS from feedback.comments");
+            // Parameters start with 1
+            
+            preparedStatement.setString(1, ID_Persona);
+            preparedStatement.setString(2, P_Nombre);
+            preparedStatement.setString(3, S_Nombre);
+            preparedStatement.setString(4, P_Apellido);
+            preparedStatement.setString(5, S_Apellido);
+            preparedStatement.setString(6, Fecha_Nacimiento);
+            preparedStatement.setString(7, genero);
+            preparedStatement.setString(8, Pais);
+            preparedStatement.setString(9, Cedula);
+            preparedStatement.executeUpdate();
+
+            preparedStatement = connect
+                    .prepareStatement("SELECT ID_Persona, P_Nombre, S_Nombre, P_Apellido, S_Apellido, Fecha_Nacimiento, Genero, Pais, Cedula from trabajofinaldb.persona");
+            resultSet = preparedStatement.executeQuery();
+            writeResultSet(resultSet);
+
+            /*// Remove again the insert comment
+            preparedStatement = connect
+            .prepareStatement("delete from feedback.comments where myuser= ? ; ");
+            preparedStatement.setString(1, "Test");
+            preparedStatement.executeUpdate();*/
+
+            resultSet = statement
+            .executeQuery("select * from trabajofinaldb.persona");
+            writeMetaData(resultSet);
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            close();
+        }
+
+    }
+	
+    private void writeMetaData(ResultSet resultSet) throws SQLException {
+        //  Now get some metadata from the database
+        // Result set get the result of the SQL query
+
+        System.out.println("The columns in the table are: ");
+
+        System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
+        for  (int i = 1; i<= resultSet.getMetaData().getColumnCount(); i++){
+            System.out.println("Column " +i  + " "+ resultSet.getMetaData().getColumnName(i));
+        }
+    }
+
+    private void writeResultSet(ResultSet resultSet) throws SQLException {
+        // ResultSet is initially before the first data set
+        while (resultSet.next()) {
+            // It is possible to get the columns via name
+            // also possible to get the columns via the column number
+            // which starts at 1
+            // e.g. resultSet.getSTring(2);
+            String id = resultSet.getString("ID_Persona");
+            String primNomb = resultSet.getString("P_Nombre");
+            String segNomb = resultSet.getString("S_Nombre");
+            String pApell = resultSet.getString("P_Apellido");
+            String sApell = resultSet.getString("S_Apellido");
+            String fNacim = resultSet.getString("Fecha_Nacimiento");
+            String gen = resultSet.getString("Genero");
+            System.out.println("ID_Persona: " + id);
+            System.out.println("P_Nombre: " + primNomb);
+            System.out.println("P_Apellido: " + pApell);
+            System.out.println("Fecha_Nacimiento: " + fNacim);
+            System.out.println("Genero: " + fNacim);
+        }
+    }
+
+    // You need to close the resultSet
+    private void close() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (statement != null) {
+                statement.close();
+            }
+
+            if (connect != null) {
+                connect.close();
+            }
+        } catch (Exception e) {
+
+        }
+    }
 }
+
+
